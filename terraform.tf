c2dce1bad80fbec399ca63e75c08e735bd7a509d
Nikola Stjelja <nstjelja@gmail.com>
2020-01-23 10:48:11 +0100

    Server 2

--- terraform.tf
+++ terraform.tf
@@ -2,6 +2,15 @@ resource "random_id" "server" {
   byte_length = 8
 }
 
+resource "random_id" "server_2" {
+  byte_length = 8
+}
+
 output "generated_random_id" {
   value = random_id.server.b64_url
 }
+
+output "generated_random_id_2" {
+  value = random_id.server_2.b64_url
+}
+
